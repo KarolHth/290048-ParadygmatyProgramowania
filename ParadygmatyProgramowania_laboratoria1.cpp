1eb4e0aa393570ba1f71b23ef93753336f6732f1
Karol Hegenbarth <100857992+KarolHth@users.noreply.github.com>
2026-03-26 21:17:21 +0100

    lab1
    nr indeksu, imie, nazwisko, obecnosc
    dodawanie, edytowanie, usuwanie użytkowników

---
+++ ParadygmatyProgramowania_laboratoria1.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int MAX = 10;
+
+void dodajOsobe(int* nrIndeksu, string* imie, string* nazwisko, bool* obecny, int* liczbaOsob, int nrIndeksuVal, const string* imieVal, const string* nazwiskoVal, bool obecnyVal)
+{
+    if(*liczbaOsob < MAX)
+    {
+        nrIndeksu[*liczbaOsob] = nrIndeksuVal;
+        imie[*liczbaOsob] = *imieVal;
+        nazwisko[*liczbaOsob] = *nazwiskoVal;
+        obecny[*liczbaOsob] = obecnyVal;
+        (*liczbaOsob)++;
+    }
+}
+void ustawObecnosc(int* nrIndeksu, bool* obecny, int* liczbaOsob, int nrIndeksuVal, bool obecnosci)
+{
+    for(int i = 0; i < *liczbaOsob; i++)
+    {
+        if(nrIndeksu[i] == nrIndeksuVal)
+        {
+            obecny[i] = obecnosci;
+        }
+    }
+}
+
+bool edytujOsobe(int* nrIndeksu, string* imie, string* nazwisko, bool* obecny, int* liczbaOsob, int nrIndeksuVal, int nowyNrIndeksu, const string* noweImie, const string* noweNazwisko, bool zmienicObecnosc, bool nowaObecnosc)
+{
+    int idx = -1;
+    for(int i = 0; i < *liczbaOsob; i++)
+    {
+        if(nrIndeksu[i] == nrIndeksuVal)
+        {
+            idx = i;
+            break;
+        }
+    }
+    if(idx == -1)
+    {
+        return false;
+    }
+
+    if(nowyNrIndeksu > 0 && nowyNrIndeksu != nrIndeksuVal)
+    {
+        for(int i = 0; i < *liczbaOsob; i++)
+        {
+            if(nrIndeksu[i] == nowyNrIndeksu)
+            {
+                return false; // Konflikt numerów indeksu
+            }
+        }
+        nrIndeksu[idx] = nowyNrIndeksu;
+    }
+
+    if(!noweImie->empty() && *noweImie != "-")
+    {
+        imie[idx] = *noweImie;
+    }
+
+    if(!noweNazwisko->empty() && *noweNazwisko != "-")
+    {
+        nazwisko[idx] = *noweNazwisko;
+    }
+
+    if(zmienicObecnosc)
+    {
+        obecny[idx] = nowaObecnosc;
+    }
+
+    return true;
+}
+
+bool usunOsobe(int* nrIndeksu, string* imie, string* nazwisko, bool* obecny, int* liczbaOsob, int nrIndeksuVal)
+{
+    int idx = -1;
+    for(int i = 0; i < *liczbaOsob; i++)
+    {
+        if(nrIndeksu[i] == nrIndeksuVal)
+        {
+            idx = i;
+            break;
+        }
+    }
+    if(idx == -1)
+    {
+        return false;
+    }
+
+    for(int i = idx; i < *liczbaOsob - 1; i++)
+    {
+        nrIndeksu[i] = nrIndeksu[i + 1];
+        imie[i] = imie[i + 1];
+        nazwisko[i] = nazwisko[i + 1];
+        obecny[i] = obecny[i + 1];
+    }
+
+    (*liczbaOsob)--;
+    return true;
+}
+
+void drukujListe(int* nrIndeksu, string* imie, string* nazwisko, bool* obecny, int* liczbaOsob)
+{
+    cout << "\nLista obecnosci:\n";
+    cout << "Nr indeksu | Imie | Nazwisko | Obecny\n";
+
+    for(int i = 0; i < *liczbaOsob; i++)
+    {
+        cout << nrIndeksu[i] << " | " << imie[i] << " | " << nazwisko[i] << " | " << (obecny[i] ? 1 : 0) << endl;
+    }
+}
+
+int main()
+{
+    int liczbaOsob = 0;
+    int nrIndeksuTab[MAX];
+    string imieTab[MAX];
+    string nazwiskoTab[MAX];
+    bool obecnyTab[MAX];
+
+    int nrIndeksuVal;
+    string imieVal;
+    string nazwiskoVal;
+    bool obecnyVal;
+    char wybor;
+
+    do
+    {
+        cout << "\nMenu:\n";
+        cout << " 1 - dodaj osobe\n";
+        cout << " 2 - edytuj osobe\n";
+        cout << " 3 - usun osobe\n";
+        cout << " 4 - pokaz liste\n";
+        cout << " 5 - zakoncz\n";
+        cout << "Wybierz opcje: ";
+        cin >> wybor;
+
+        switch(wybor)
+        {
+            case '1':
+            {
+                cout << "Podaj nr indeksu: ";
+                cin >> nrIndeksuVal;
+                bool existsIndex = false;
+                for(int i = 0; i < liczbaOsob; i++)
+                {
+                    if(nrIndeksuTab[i] == nrIndeksuVal)
+                    {
+                        existsIndex = true;
+                        break;
+                    }
+                }
+
+                if(existsIndex)
+                {
+                    cout << "Ten nr indeksu jest juz zajety\n";
+                    break;
+                }
+
+                cout << "Podaj imie: ";
+                cin >> imieVal;
+                cout << "Podaj nazwisko: ";
+                cin >> nazwiskoVal;
+                cout << "Czy osoba byla obecna? (1 tak, 0 nie): ";
+                cin >> obecnyVal;
+
+                dodajOsobe(nrIndeksuTab, imieTab, nazwiskoTab, obecnyTab, &liczbaOsob, nrIndeksuVal, &imieVal, &nazwiskoVal, obecnyVal);
+                break;
+            }
+            case '2':
+            {
+                cout << "Podaj nr indeksu osoby do edycji: ";
+                cin >> nrIndeksuVal;
+
+                int nowyNrIndeksu;
+                string noweImie;
+                string noweNazwisko;
+                int obecnoscInput;
+                bool zmienicObecnosc = false;
+                bool nowaObecnosc = false;
+
+                cout << "Nowy nr indeksu (0 zostaw bez zmian): ";
+                cin >> nowyNrIndeksu;
+                cout << "Nowe imie (wpisz '-' aby zostawic): ";
+                cin >> noweImie;
+                cout << "Nowe nazwisko (wpisz '-' aby zostawic): ";
+                cin >> noweNazwisko;
+                cout << "Nowa obecnosc (1 tak 0 nie -1 zostaw bez zmian): ";
+                cin >> obecnoscInput;
+
+                if(obecnoscInput == 0 || obecnoscInput == 1)
+                {
+                    zmienicObecnosc = true;
+                    nowaObecnosc = (obecnoscInput == 1);
+                }
+
+                bool ok = edytujOsobe(nrIndeksuTab, imieTab, nazwiskoTab, obecnyTab, &liczbaOsob, nrIndeksuVal, nowyNrIndeksu, &noweImie, &noweNazwisko, zmienicObecnosc, nowaObecnosc);
+                if(!ok)
+                {
+                    cout << "Nie znaleziono osoby lub nr indeksu docelowy jest zajety.\n";
+                }
+                break;
+            }
+            case '3':
+            {
+                cout << "Podaj nr indeksu osoby do usuniecia: ";
+                cin >> nrIndeksuVal;
+                bool ok = usunOsobe(nrIndeksuTab, imieTab, nazwiskoTab, obecnyTab, &liczbaOsob, nrIndeksuVal);
+                if(!ok)
+                {
+                    cout << "Nie znaleziono osoby o tym numerze indeksu.\n";
+                }
+                break;
+            }
+            case '4':
+                drukujListe(nrIndeksuTab, imieTab, nazwiskoTab, obecnyTab, &liczbaOsob);
+                break;
+            case '5':
+                cout << "Koniec.\n";
+                break;
+            default:
+                cout << "Nieprawidlowy wybor.\n";
+        }
+
+    } while(wybor != 'q' && wybor != 'Q');
+
+    return 0;
+}
